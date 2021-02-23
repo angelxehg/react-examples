06c7b13cdc10d44af795e07592edb15682c794e3
Angel Hurtado <contact@angelxehg.com>
2021-02-23 11:41:49 -0600

    Mostrar props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 class Hello extends Component {
@@ -9,12 +9,29 @@ class Hello extends Component {
   }
 }
 
+class Text extends Component {
+  
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const mostrarBoolean = this.props.boolean ? 'Cierto':'Falso';
+    // eslint-disable-next-line react/prop-types
+    return <>
+      {/* eslint-disable-next-line react/prop-types*/}
+      <p>{this.props.text}</p>
+      {/* eslint-disable-next-line react/prop-types*/}
+      <p>{this.props.number}</p>
+      <p>{mostrarBoolean}</p>
+    </>;
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello title="Hola mundo"/>
+        <Hello title="Hola mundo" />
+        <Text text="Texto" number={1} boolean={false} />
       </header>
     </div>
   );
