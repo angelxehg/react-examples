2cea67c31cdbed18ee9039bfa952601c206fcd81
Angel Hurtado <contact@angelxehg.com>
2021-02-23 12:23:23 -0600

    Componente con defaultProps

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
-import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
+import PropTypes from "prop-types"
 import './App.css';
 
-class Hello extends Component {
+class Title extends Component {
   render() {
-    // eslint-disable-next-line react/prop-types
-    return <p>{this.props.title}</p>;
+    const {text} = this.props;
+    return <h1>{text}</h1>;
   }
 }
 
-class Text extends Component {
+Title.propTypes = {
+  text: PropTypes.string
+}
 
-  render() {
-    // eslint-disable-next-line react/prop-types
-    const { arrayOfNumbers, multiply, objectWithKey, title } = this.props;
-    // eslint-disable-next-line react/prop-types
-    const mapped = arrayOfNumbers.map(multiply);
-    return <>
-      {title}
-      <p>{mapped.join(', ')}</p>
-      {/* eslint-disable-next-line react/prop-types */}
-      <p>{objectWithKey.key}</p>
-    </>;
-  }
+Title.defaultProps = {
+  text: 'Título por defecto'
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Hello title="Hola mundo" />
-        <Text
-          arrayOfNumbers={[1,2,3,4,5]}
-          objectWithKey={{key: 'value'}}
-          multiply={(n) => n * 4}
-          title={<h1>Hola</h1>}
-        />
-      </header>
+      <Title text="Hola mundo"/>
     </div>
   );
 }
