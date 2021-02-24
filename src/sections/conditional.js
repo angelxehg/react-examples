12e179da395b7322111f8c7ea4df4f0b307d743c
Angel Hurtado <contact@angelxehg.com>
2021-02-23 21:15:20 -0600

    Renderizado condicional

--- src/sections/conditional.js
+++ src/sections/conditional.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ComponenteA = () => <p>Componente A</p>
 const ComponenteB = () => <p>Componente B</p>
 
 const Conditional = () => {
+  const [mostrarA] = useState(false);
+
+  const selectCom = (mostrarA) => {
+    if (mostrarA) {
+      return <ComponenteA/>;
+    }
+    return <ComponenteB/>;
+  }
+
   return (<>
     <h1>Condicional Rendering</h1>
-    <ComponenteA/>
-    <ComponenteB/>
+    {selectCom(mostrarA)}
   </>);
 }
 
