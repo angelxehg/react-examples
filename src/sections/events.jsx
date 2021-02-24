4b44bdba78689e4ec6b101b59e37d0651d5004f4
Angel Hurtado <contact@angelxehg.com>
2021-02-24 11:37:26 -0600

    Capturar evento click

--- src/sections/events.jsx
+++ src/sections/events.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Eventos = () => {
+  const handleClick = (e) => {
+    console.log(e); // Evento sintetico
+    console.log(e.nativeEvent); // Evento nativo
+    alert('Me diste clic')
+  }
   return <>
     <h1>Eventos</h1>
-    <button className="btn btn-primary" onClick={() => alert('Me diste clic')}>
+    <button className="btn btn-primary" onClick={handleClick}>
       Dame clic
     </button>
   </>;
