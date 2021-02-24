f9682225c6f401cd8e5001eaa79f672229194cf9
Angel Hurtado <contact@angelxehg.com>
2021-02-23 21:49:08 -0600

    Componente de Autos

--- src/sections/cars.js
+++ src/sections/cars.js
@@ -1,16 +1,27 @@
-import { React } from "react";
-import autos from "../data/autos"
+import React from "react";
+import PropTypes from "prop-types"
+
+import autos from "../data/autos";
+
+const Auto = ({ car }) => {
+  return <li>
+    <p><strong>Nombre:</strong> {car.name}</p>
+    <p><strong>Marca:</strong> {car.brand}</p>
+  </li>
+}
+
+Auto.propTypes = {
+  car: PropTypes.object,
+}
 
 const ListaAutos = () => {
   return <>
     <h1>Lista de autos</h1>
     <ul>
-      {autos.map(auto => {
-        return <li key={auto.id}>
-          <p><strong>Nombre:</strong> {auto.name}</p>
-          <p><strong>Marca:</strong> {auto.brand}</p>
-        </li>
-      })}
+      {autos.map(a => <Auto
+        key={a.id}
+        car={a}
+      />)}
     </ul>
   </>
 };
