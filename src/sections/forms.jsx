e3201990506e57777997e11cdeff883b2cfc82bc
Angel Hurtado <contact@angelxehg.com>
2021-02-24 12:05:01 -0600

    Usar referencias (no es declarativo)

--- src/sections/forms.jsx
+++ src/sections/forms.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formularios = () => {
+
+  const [inputName, setInputName] = useState();
+  const [inputTwitter, setInputTwitter] = useState();
   
   const handleClick = (e) => {
     e.preventDefault();
-    const name = document.getElementById('name').value;
-    const twitter = document.getElementById('twitter').value;
+    const name = inputName.value;
+    const twitter = inputTwitter.value;
     console.log({name, twitter});
   }
 
@@ -14,11 +17,11 @@ const Formularios = () => {
     <form>
       <p>
         <label htmlFor="name">Nombre:</label>
-        <input id="name" type="text" placeholder="Introduce tu nombre"/>
+        <input id="name" type="text" placeholder="Introduce tu nombre" ref={setInputName}/>
       </p>
       <p>
         <label htmlFor="twitter">Twitter:</label>
-        <input id="twitter" type="text" placeholder="Introduce tu twitter"/>
+        <input id="twitter" type="text" placeholder="Introduce tu twitter" ref={setInputTwitter}/>
       </p>
       <button onClick={handleClick}>Enviar</button>
     </form>
