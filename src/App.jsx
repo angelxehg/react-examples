cd975817fa8651314b612bbb0435a157bc0873a2
Angel Hurtado <contact@angelxehg.com>
2021-03-03 11:56:05 -0600

    Crear seccion LifeCycles

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,13 @@ import Eventos from './sections/events';
 import Contador from './sections/contador';
 import Formularios from './sections/forms';
 import Components from './sections/components';
+import LifeCycles from './sections/lifecycles';
 
 function App() {
   return (
     <div className="container-sm pt-5 pb-5">
       <h1>Udemy Curso React <span className="badge bg-secondary">v0.1.3</span></h1>
+      <LifeCycles/>
       <Components/>
       <Formularios/>
       <Eventos/>
---
+++ src/sections/lifecycles.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LifeCycles extends Component {
+  render() {
+    return (
+      <div className="mt-5">
+        <h2>Ciclos de vida</h2>
+      </div>
+    )
+  }
+}
+
+export default LifeCycles
