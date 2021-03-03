3620ddb755da53177cb35c50aa23dcdccdfa7490
Angel Hurtado <contact@angelxehg.com>
2021-03-03 12:07:04 -0600

    Creación de componente

--- src/sections/lifecycles.jsx
+++ src/sections/lifecycles.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 
 class LifeCycles extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { message: 'Mensaje inicial' };
+  }
+
+  handleClick = () => {
+    this.setState({ message: 'Mensaje cambiado' });
+  }
+
   render() {
     return (
       <div className="mt-5">
         <h2>Ciclos de vida</h2>
+        <p>{this.state.message}</p>
+        <button
+          className="btn btn-secondary"
+          onClick={this.handleClick}>
+          Clic
+        </button>
       </div>
     )
   }
