892051e5bff97e32240b35b47a9361552e9519f4
Angel Hurtado <contact@angelxehg.com>
2021-03-06 16:44:37 -0600

    Usar una tabla compuesta

--- src/sections/bitcoin.jsx
+++ src/sections/bitcoin.jsx
@@ -1,16 +1,58 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+const Table = ({ children }) => (
+  <table className="table table-sm">
+    {children}
+  </table>
+)
+
+Table.propTypes = {
+  children: PropTypes.node
+}
+
+const TableHeader = ({ headers }) => (
+  <thead>
+    <tr>
+      {headers.map(header => <th key={header} scope="col">{header}</th>)}
+    </tr>
+  </thead>
+)
+
+TableHeader.propTypes = {
+  headers: PropTypes.arrayOf(PropTypes.string)
+}
+
+const TableBody = ({ children }) => <tbody>{children}</tbody>
+
+TableBody.propTypes = {
+  children: PropTypes.node
+}
+
+const CurrencyRow = ({ currency, value }) => (
+  <tr>
+    <th scope="row">{currency}</th>
+    <td>{value}</td>
+  </tr>
+)
+
+CurrencyRow.propTypes = {
+  currency: PropTypes.string,
+  value: PropTypes.number
+}
 
 class BitcoinPage extends Component {
 
   state = { bpi: {} }
 
   componentDidMount() {
-    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(res => res.json())
-      .then(data => {
-        const { bpi } = data;
-        this.setState({ bpi });
-      });
+    this.fetchPrice();
+  }
+
+  fetchPrice = async () => {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+    this.setState({ bpi: data.bpi });
   }
 
   render() {
@@ -18,7 +60,13 @@ class BitcoinPage extends Component {
     return (
       <div className="mt-5">
         <h2>BTC (Container-Content)</h2>
-        {Object.keys(bpi).map(key => <p key={key} >1 BTC = {bpi[key].rate} {key}</p>)}
+        <p>1 BTC equivale a...</p>
+        <Table>
+          <TableHeader headers={['Moneda', 'Precio']} />
+          <TableBody>
+            {Object.keys(bpi).map(key => <CurrencyRow key={key} currency={key} value={bpi[key].rate_float} />)}
+          </TableBody>
+        </Table>
       </div>
     )
   }
